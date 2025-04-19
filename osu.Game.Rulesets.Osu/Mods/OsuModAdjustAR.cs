4970350ca80c46106ba77620bbb4f8f08b4255d5
SuperChick <88729655+SuperChick1705@users.noreply.github.com>
2025-04-19 10:48:28 +0200

    Update OsuModAdjustAR.cs

--- osu.Game.Rulesets.Osu/Mods/OsuModAdjustAR.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModAdjustAR.cs
@@ -1,30 +1,34 @@
+using osu.Framework.Bindables;
+using osu.Framework.Localisation;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.UI;
-using osu.Game.Configuration;
+using System;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModAdjustAR : Mod, IApplicableToDifficulty, IReadFromConfig
+    public class OsuModAdjustAR : Mod, IApplicableToDifficulty
     {
         public override string Name => "Adjust AR";
         public override string Acronym => "AR";
+        public override LocalisableString Description => "Adjust approach rate";
         public override ModType Type => ModType.DifficultyReduction;
         public override double ScoreMultiplier => 0.5;
-        
+
         [SettingSource("AR Value", "Set custom approach rate")]
         public BindableFloat CustomAR { get; } = new BindableFloat
         {
             MinValue = -10,
             MaxValue = 11,
-            Default = 5, // Default to normal AR5
+            Default = 5,
             Precision = 0.1f
         };
 
         public void ApplyToDifficulty(BeatmapDifficulty difficulty)
         {
-            // Completely override the original AR
+            // Directly override the AR value
             difficulty.ApproachRate = CustomAR.Value;
         }
     }
